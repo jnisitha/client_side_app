8cdab4cc3d8e34864b4e295d51583aff32781d58
Nisitha Jayatilleka <jnisitha@gmail.com>
2019-02-21 13:30:31 -0500

    Implement simple search bar logic

---
+++ lib/home.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeBody(),
+      bottomNavigationBar: BottomAppBar(),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        AppBar(
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                showSearch(context: context, delegate: SearchBar());
+              },
+            )
+          ],
+        ),
+        ButtonBar(
+          alignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: const Text('Request'),
+            ),
+            RaisedButton(
+              child: const Text('Request'),
+            ),
+            RaisedButton(
+              child: const Text('Request'),
+            ),
+          ],
+        ),
+        //ListView()
+      ],
+    );
+  }
+}
+
+class SearchBar extends SearchDelegate<String> {
+  final communityNames = [
+    "Fairview",
+    "Fairbank",
+    "Thatcher",
+    "Colombus",
+    "Bulbasaur",
+    "Charmander"
+  ];
+  final recentCommunityNames = ["Fairbank", "Thatcher"];
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+          icon: Icon(Icons.clear),
+          onPressed: () {
+            query = "";
+          })
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) { //This should direct to whatever route the query gives.
+    return Container(
+        height: 100.0,
+        width: 100.0,
+        child: Card(
+          color: Colors.red,
+          child: Center(
+            child: Text(query),
+          ),
+        ));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final suggestionList = query.isEmpty
+        ? recentCommunityNames
+        : communityNames.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+            onTap: () {
+              showResults(context); //This takes you to build results.
+            },
+            leading: Icon(Icons.location_city),
+            title: RichText(
+                text: TextSpan(
+                    text: suggestionList[index].substring(0, query.length),
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                    children: [
+                  TextSpan(
+                      text: suggestionList[index].substring(query.length),
+                      style: TextStyle(color: Colors.grey))
+                ])),
+          ),
+      itemCount: suggestionList.length,
+    );
+  }
+}
+
+class BottomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 56.0,
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        decoration: BoxDecoration(color: Colors.lightBlue),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.search),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.favorite),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.message),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.supervisor_account),
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                icon: Icon(Icons.add),
+              ),
+            ),
+          ],
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//import 'package:scoped_model/scoped_model.dart';
+
+import 'home.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -8,124 +13,6 @@ void main() {
   ));
 }
 
-class BottomAppBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 56.0,
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        decoration: BoxDecoration(color: Colors.lightBlue),
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.search),
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.favorite),
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.message),
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.supervisor_account),
-              ),
-            ),
-            Expanded(
-              child: IconButton(
-                icon: Icon(Icons.add),
-              ),
-            ),
-          ],
-        ));
-  }
-}
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-//      appBar: AppBar(
-//        leading:IconButton(
-//          icon: Icon(Icons.menu),
-//          tooltip: 'Navigation menu',
-//          onPressed: null,
-//        ),
-//        title: Text("Example"),
-//        actions: <Widget>[
-//          IconButton(
-//            icon: Icon(Icons.search),
-//            tooltip: 'Search',
-//            onPressed: null,
-//          ),
-//        ],
-//      ),
-      body: Center(
-        child: Counter(),
-      ),
-      bottomNavigationBar: BottomAppBar(),
-//      floatingActionButton: FloatingActionButton(
-//        tooltip: 'Add',
-//        child: Icon(Icons.add),
-//        onPressed: null,
-//      ),
-    );
-  }
-}
 
-class Counter extends StatefulWidget {
-  // This class is the configuration for the state. It holds the
-  // values (in this case nothing) provided by the parent and used by the build
-  // method of the State. Fields in a Widget subclass are always marked "final".
 
-  @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      // This call to setState tells the Flutter framework that
-      // something has changed in this State, which causes it to rerun
-      // the build method below so that the display can reflect the
-      // updated values. If we changed _counter without calling
-      // setState(), then the build method would not be called again,
-      // and so nothing would appear to happen.
-      _counter++;
-      print('in setState');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance
-    // as done by the _increment method above.
-    // The Flutter framework has been optimized to make rerunning
-    // build methods fast, so that you can just rebuild anything that
-    // needs updating rather than having to individually change
-    // instances of widgets.
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Center(
-            child: RaisedButton(
-                onPressed: _increment,
-                child: Text('Increment'),
-                color: Colors.blue,
-                padding: EdgeInsets.symmetric(horizontal: 10.0))),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15.0)
-        ),
-        Text('Count: $_counter', textAlign: TextAlign.center),
-      ],
-    );
-  }
-}
