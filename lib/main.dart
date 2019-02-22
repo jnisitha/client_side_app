9134a675aa60c1a466a1cf030fd96ef14113d9e7
Nisitha Jayatilleka <jnisitha@gmail.com>
2019-02-21 21:58:12 -0500

    Added RESOURCES.TXT

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,8 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
+  // that it has a State object (deVISUAL DEBUGGING: https://flutter.io/docs/testing/debugging#visual-debugging
+  //LAYOUT WIDGETS (ALL BY FLUTTER): https://flutter.io/docs/development/ui/widgets/layoutfined below) that contains fields that affect
   // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
